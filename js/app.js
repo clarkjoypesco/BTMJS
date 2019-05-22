577742eafd353519cb906f000d7b126785ef5e08
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-23 07:07:27 +0800

    var let and const

--- js/app.js
+++ js/app.js
@@ -1,34 +1,90 @@
 // alert("Hello World");
 
 // Log To Console
-console.log("Hello World");
-console.log(123);
-console.log(true);
+// console.log("Hello World");
+// console.log(123);
+// console.log(true);
 
-var greeting = "Hello";
-console.log(greeting);
+// var greeting = "Hello";
+// console.log(greeting);
 
-console.log([1, 2, 3, 4, 5]);
-console.log({ a: 1, b: 2 });
-console.table({ a: 1, b: 2 });
+// console.log([1, 2, 3, 4, 5]);
+// console.log({ a: 1, b: 2 });
+// console.table({ a: 1, b: 2 });
 
-console.error("This is some error");
+// console.error("This is some error");
 
-console.clear();
+// console.clear();
 
-console.warn("This is a warning!");
-console.time("Hello");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.log("Hello World");
-console.timeEnd("Hello");
+// console.warn("This is a warning!");
+// console.time("Hello");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.timeEnd("Hello");
 /*
 multi 
 line c
 comments
 */
+
+//var, let, const
+
+// var name = "John Doe";
+// console.log(name);
+
+// name = "Steve Smith";
+// console.log(name);
+
+// //init var
+// var greeting;
+
+// console.log(greeting);
+// greeting = "Hello";
+// console.log(greeting);
+
+// //letters, numbers,_, $
+// //Can not start with number
+// // var name = 'John';
+
+// var firstName = 'John';//Camel Case
+// var first_name ='Sara';//underscore convention
+// var FirstName = 'Tom'; //Pascal Case
+// var firstname;
+
+//LET
+// let name;
+// let name = "John Doe";
+// console.log(name);
+
+// name = "Steve Smith";
+// console.log(name);
+
+//CONST
+const name = "John";
+console.log(name);
+
+//cannot reassign
+// name = "Sarah";
+
+// const greeting;
+
+const person = {
+  name: "John",
+  age: 30
+};
+
+person.name = "Sara";
+person.age = 32;
+
+// console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+
+console.log(numbers);
