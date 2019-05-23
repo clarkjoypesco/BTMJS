f9948d49e6c6735a127580b3ebb7d018557b44db
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-23 12:29:26 +0800

    switches

--- js/app.js
+++ js/app.js
@@ -1,88 +1,46 @@
-// if(something){
-//   do something
-// } else {
-//   do something else
-// }
+const color = "yellow";
 
-const id = 100;
-
-// //UQUAL TO
-// if (id == 101) {
-//   console.log("CORRECT");
-// } else {
-//   console.log("INCORRECT");
-// }
-
-// //NOT EQUAL TO
-// if (id != 101) {
-//   console.log("CORRECT");
-// } else {
-//   console.log("INCORRECT");
-// }
-
-// //EQUAL TO VALUE & TYPE
-// if (id === 100) {
-//   console.log("CORRECT");
-// } else {
-//   console.log("INCORRECT");
-// }
-
-// //EQUAL TO VALUE & TYPE
-// if (id !== 100) {
-//   console.log("CORRECT");
-// } else {
-//   console.log("INCORRECT");
-// }
-
-//Test if undefined
-// if (typeof id !== "undefined") {
-//   console.log(`The ID is ${id}`);
-// } else {
-//   console.log("NO ID");
-// }
-
-//GREATER OR LESS THAN
-// if (id <= 100) {
-//   console.log("CORRECT");
-// } else {
-//   console.log("INCORRECT");
-// }
-
-//if else
-// const color = "yellow";
-
-// if (color === "red") {
-//   console.log("Color is Red");
-// } else if (color === "blue") {
-//   console.log("Color is blue");
-// } else {
-//   console.log("Color is not red or  blue");
-// }
-
-//LOGICAL OPERATORS
-
-const name = "Steve";
-const age = 20;
-
-if (age > 0 && age < 12) {
-  console.log(`${name} is a child`);
-} else if (age >= 13 && age <= 19) {
-  console.log(`${name} is a teenager`);
-} else {
-  console.log(`${name} is an adult`);
+switch (color) {
+  case "red":
+    console.log("Color is red");
+    break;
+  case "blue":
+    console.log("Color is blue");
+    break;
+  default:
+    console.log("Color is not red or blue");
+    break;
 }
 
-// OR ||
+let day;
 
-if (age < 16 || age > 65) {
-  console.log(`${name} can not run in race`);
-} else {
-  console.log(`${name} is registered for the race`);
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+
+  case 1:
+    day = "Monday";
+    break;
+
+  case 2:
+    day = "Tuesday";
+    break;
+
+  case 3:
+    day = "Wednesday";
+    break;
+
+  case 4:
+    day = "Thursday";
+    break;
+
+  case 5:
+    day = "Friay";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
 }
 
-//TERNARY OPERATORS
-console.log(id === 100 ? "CORRECT" : "INCORRECT");
-
-//Without brace
-if (id === 100) console.log("CORRECT");
-else console.log("INCORRECT");
+console.log(`Today is ${day}`);
