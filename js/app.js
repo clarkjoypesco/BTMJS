6fb1441bf71e5ec09406763c6c97547c9b9ce2f5
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-23 09:15:31 +0800

    Object Literals

--- js/app.js
+++ js/app.js
@@ -1,42 +1,77 @@
-const name = "John";
-const age = 31;
-const job = "Web Developer";
-const city = "Miami";
-let html;
+//Create some arrays
+const numbers = [43, 56, 33, 23, 44, 36, 5];
+const numbers2 = new Array(22, 45, 33, 76, 54);
+const fruit = ["apple", "banana", "orange", "pear"];
+const mixed = [22, "hello", true, undefined, null, { a: 1, b: 1 }, new Date()];
 
-//without template strings (es5)
+let val;
 
-html =
-  "<ul> " +
-  "<li>Name: " +
-  name +
-  "</li>" +
-  "<li>Age: " +
-  age +
-  "</li>" +
-  "<li>Job: " +
-  job +
-  "</li>" +
-  "<li>City: " +
-  city +
-  "</li>" +
-  "</ul>";
+//Get array length
+val = numbers.length;
 
-function hello() {
-  return "hello";
+//Check if is array
+val = Array.isArray(numbers);
+
+//Get single value
+val = numbers[3];
+val = numbers[0];
+
+//Insert into array
+numbers[2] = 100;
+
+//find index of value
+val = numbers.indexOf(36);
+
+//MUTATING ARARYS
+
+//Add on to end
+numbers.push(250);
+
+//Add on to front
+numbers.unshift(120);
+
+//Take off from end
+numbers.pop();
+
+//Take of from front
+numbers.shift();
+
+//Splice values
+numbers.splice(1, 3);
+
+//Reverse
+numbers.reverse();
+
+//concatenate array
+val = numbers.concat(numbers2);
+
+//Sorting arrays
+val = fruit.sort();
+val = numbers.sort();
+
+//Use the "compare function"
+
+val = numbers.sort(function(x, y) {
+  return x - y;
+});
+
+//Reverse sort
+val = numbers.sort(function(x, y) {
+  return y - x;
+});
+
+//Find
+
+function under50(num) {
+  return num < 50;
 }
-// With template strings or literals (es6)
+function over50(num) {
+  return num > 50;
+}
 
-html = `
-<ul>
-<li>Name: ${name}</li>
-<li>Age: ${age}</li>
-<li>Job: ${job}</li>
-<li>${2 + 2}</li>
-<li>${hello()}</li>
-<li>${age > 30 ? "Over 30" : "Under 30"}</li>
+val = numbers.find(under50);
+val = numbers.find(over50);
 
-</ul>
-`;
-
-document.body.innerHTML = html;
+//output
+console.log(numbers);
+console.log(val);
