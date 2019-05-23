d7ea694c4fc074d77e26bee01b54ecc8680bd68a
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-24 06:24:34 +0800

    DOM Selectors for Single Elements

--- 03_dom_manipulations_events/js/app.js
+++ 03_dom_manipulations_events/js/app.js
@@ -1,42 +1,31 @@
-let val;
+//document.getElementById()
+console.log(document.getElementById("task-title"));
 
-val = document;
-val = document.all;
-val = document.all[0];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+//Get things from the element
+console.log(document.getElementById("task-title").id);
+console.log(document.getElementById("task-title").className);
 
-//not recommended
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+const taskTitle = document.getElementById("task-title");
+// Change styling
+taskTitle.style.background = "#333";
+taskTitle.style.color = "#fff";
+taskTitle.style.padding = "5px";
+// taskTitle.style.display = "none";
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
+//Change content
+taskTitle.textContent = "Task List";
+taskTitle.innerText = "My Tasks";
+taskTitle.innerHTML = '<span style="color:red">Task List</span>';
 
-val = document.images;
+//document.querySelector()
+console.log(document.querySelector("#task-title"));
+console.log(document.querySelector(".card-title"));
+console.log(document.querySelector("h5"));
 
-val = document.scripts;
-val = document.scripts[2].getAttribute("src");
-
-let scripts = document.scripts;
-
-let scriptsArr = Array.from(scripts);
-
-scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute("src"));
-});
-
-//output
-console.log(val);
+document.querySelector("li").style.color = "red";
+document.querySelector("ul li").style.color = "blue";
+document.querySelector("li:last-child").style.color = "yellow";
+document.querySelector("li:nth-child(3)").style.color = "violet";
+document.querySelector("li:nth-child(4)").textContent = "Hello World";
+document.querySelector("li:nth-child(odd)").style.background = "#ccc";
+document.querySelector("li:nth-child(even)").style.background = "#f4f4f4";
