385f5663ff350cc31673a4b723bc2df512422a8c
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-24 23:06:59 +0800

    Loan Calc - Loader User Experience

--- 04_dom_projects/02_loan_calc/js/app.js
+++ 04_dom_projects/02_loan_calc/js/app.js
@@ -1,12 +1,20 @@
 //Listen for submit
 
-document
-  .getElementById("loan-form")
-  .addEventListener("submit", calculateResults);
+document.getElementById("loan-form").addEventListener("submit", function(e) {
+  //Hide Results
+  document.getElementById("results").style.display = "none";
+
+  //Show loader
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 //Calculate results
 
-function calculateResults(e) {
+function calculateResults() {
   console.log("Calculating...");
 
   //UI Vars
@@ -29,11 +37,18 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    //Show results
+    document.getElementById("results").style.display = "block";
+    //Hide loader
+    document.getElementById("loading").style.display = "none";
   } else {
+    //Show results
+    document.getElementById("results").style.display = "none";
+    //Hide loader
+    document.getElementById("loading").style.display = "none";
     showError("Please check your numbers");
   }
-
-  e.preventDefault();
 }
 
 //Show Error
