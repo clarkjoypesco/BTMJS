4b21e6688361a088e412c818e80cf5a205eee2e7
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-25 11:49:58 +0800

    ES6 Classes

--- 05_oo_jses5/js/app.js
+++ 05_oo_jses5/js/app.js
@@ -1,27 +1,33 @@
-const personPrototypes = {
-  greeting: function() {
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthday = new Date(dob);
+  }
+
+  greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
-  },
-  getsMarried: function(newLastName) {
+  }
+
+  calculateAge() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1978);
+  }
+
+  getsMarried(newLastName) {
     this.lastName = newLastName;
   }
-};
 
-const mary = Object.create(personPrototypes);
+  static addNumbers(x, y) {
+    return x + y;
+  }
+}
 
-mary.firstName = "Mary";
-mary.lastName = "Williams";
-mary.age = 30;
+const mary = new Person("Mary", "Smith", "10-3-2003");
 mary.getsMarried("Thompson");
-
 console.log(mary);
+
 console.log(mary.greeting());
-
-const cj = Object.create(personPrototypes, {
-  firstName: { value: "CJ" },
-  lastName: { value: "Pesco" },
-  age: { value: 36 }
-});
-
-console.log(cj);
-console.log(cj.greeting());
+console.log(mary.calculateAge());
+console.log(Person.addNumbers(1, 2));
