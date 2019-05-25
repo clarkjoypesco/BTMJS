d9cb8963252cab6dcf8c20aaacaec113b943b2f2
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-25 10:53:14 +0800

    Prototypal Inheritance

--- 05_oo_jses5/js/app.js
+++ 05_oo_jses5/js/app.js
@@ -1,38 +1,45 @@
-// Object.prototype
-// Person.prototype
-
-//Person constructor
-function Person(firstName, lastName, dob) {
+//Person Constructor
+function Person(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.birthday = new Date(dob);
 }
 
-//Calculate age
-Person.prototype.calculateAge = function() {
-  const diff = Date.now() - this.birthday.getTime();
-  const ageDate = new Date(diff);
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
+//Greeting
+Person.prototype.greeting = function() {
+  return `Hello there ${this.firstName} ${this.lastName}`;
 };
 
-//Get Full name
-Person.prototype.getFullName = function() {
-  return `${this.firstName} ${this.lastName}`;
+const person1 = new Person("John", "Doe");
+
+// console.log(person1.greeting());
+
+// Customer constructor
+function Customer(firstName, lastName, phone, membership) {
+  Person.call(this, firstName, lastName);
+
+  this.phone = phone;
+  this.membership = membership;
+}
+
+//Inherit the Person prototype methods
+
+Customer.prototype = Object.create(Person.prototype);
+
+//Make customer.prototype return Customer()
+
+Customer.prototype.constructor = Customer;
+
+//Create a customer
+
+const customer1 = new Customer("Tom", "Smith", "+638289291", "Standard");
+
+console.log(customer1);
+
+//Customer greeting
+Customer.prototype.greeting = function() {
+  return `Hello there ${this.firstName} ${
+    this.lastName
+  } welcome to our company`;
 };
 
-//Gets Married
-Person.prototype.getsMarried = function(newLastName) {
-  this.lastName = newLastName;
-};
-
-const john = new Person("John", "Doe", "12-8-1993");
-const mary = new Person("Mary", "Johnson", "March 20 1978");
-
-console.log(mary);
-console.log(john.calculateAge());
-console.log(mary.getFullName());
-mary.getsMarried("Smith");
-console.log(mary.getFullName());
-
-console.log(mary.hasOwnProperty("firstName"));
-console.log(mary.hasOwnProperty("getFullName"));
+console.log(customer1.greeting());
