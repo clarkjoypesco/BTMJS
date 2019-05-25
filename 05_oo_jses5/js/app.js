0844c5b4f291066597f5ece2fa69360be9e1c985
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-25 11:59:28 +0800

    Sub Classes

--- 05_oo_jses5/js/app.js
+++ 05_oo_jses5/js/app.js
@@ -1,33 +1,29 @@
 class Person {
-  constructor(firstName, lastName, dob) {
+  constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.birthday = new Date(dob);
   }
 
   greeting() {
     return `Hello there ${this.firstName} ${this.lastName}`;
   }
+}
 
-  calculateAge() {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1978);
+class Customer extends Person {
+  constructor(firstName, lastName, phone, membership) {
+    super(firstName, lastName);
+
+    this.phone = phone;
+    this.membership = membership;
   }
 
-  getsMarried(newLastName) {
-    this.lastName = newLastName;
-  }
-
-  static addNumbers(x, y) {
-    return x + y;
+  static getMembershipCost() {
+    return 500;
   }
 }
 
-const mary = new Person("Mary", "Smith", "10-3-2003");
-mary.getsMarried("Thompson");
-console.log(mary);
+const john = new Customer("John", "Doe", "555-555-555", "Standard");
 
-console.log(mary.greeting());
-console.log(mary.calculateAge());
-console.log(Person.addNumbers(1, 2));
+console.log(john.greeting());
+
+console.log(Customer.getMembershipCost());
