38d59344eab82046a51b38ffec8c5e6e1b88cc8b
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-25 20:15:41 +0800

    Working with Ajax JSON

--- 07_async_js_ajax_fetchapi/js/app.js
+++ 07_async_js_ajax_fetchapi/js/app.js
@@ -1,52 +1,63 @@
-document.getElementById("button").addEventListener("click", loadData);
+document.getElementById("button1").addEventListener("click", loadCustomer);
 
-function loadData() {
-  // Create an XHR Object
+document.getElementById("button2").addEventListener("click", loadCustomers);
 
+//Load Customer
+function loadCustomer() {
   const xhr = new XMLHttpRequest();
 
-  // OPEN
-  xhr.open("GET", "data.txt", true);
-
-  //   console.log("READYSTATE", xhr.readyState);
-
-  // Optional - Used for spinners/loaders
-  xhr.onprogress = function() {
-    console.log("READYSTATE", xhr.readyState);
-  };
+  xhr.open("GET", "customer.json", true);
 
   xhr.onload = function() {
-    console.log("READYSTATE", xhr.readyState);
     if (this.status === 200) {
       //   console.log(this.responseText);
-      document.getElementById("output").innerHTML = `<h1>${
-        this.responseText
-      }</h1>`;
+
+      const customer = JSON.parse(this.responseText);
+
+      const output = `
+      <ul>
+      <li>ID: ${customer.id}</li>
+      <li>Name: ${customer.name}</li>
+      <li>Company: ${customer.company}</li>
+      <li>Phone: ${customer.phone}</li>
+      </ul>
+      `;
+
+      document.getElementById("customer").innerHTML = output;
     }
   };
 
-  //   xhr.onreadystatechange = function() {
-  //     console.log("READYSTATE", xhr.readyState);
-  //     if (this.status === 200 && this.readyState === 4) {
-  //       console.log(this.responseText);
-  //     }
-  //   };
+  xhr.send();
+}
 
-  xhr.onerror = function() {
-    console.log("Request error...");
+//Load Customers
+
+function loadCustomers(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "customers.json", true);
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+      //   console.log(this.responseText);
+
+      const customers = JSON.parse(this.responseText);
+
+      let output = "";
+      customers.forEach(function(customer) {
+        output += `
+        <ul>
+        <li>ID: ${customer.id}</li>
+        <li>Name: ${customer.name}</li>
+        <li>Company: ${customer.company}</li>
+        <li>Phone: ${customer.phone}</li>
+        </ul>
+        `;
+      });
+
+      document.getElementById("customers").innerHTML = output;
+    }
   };
 
   xhr.send();
-
-  //READYSTATE values
-  // 0: request not initiatilized
-  // 1: server connection established
-  // 2:  request received
-  // 3: processing request
-  // 4: request finished and response is ready
-
-  //HTTP STATUSES
-  //200: 'ok'
-  //403: 'forbidden'
-  //404: 'not foud'
 }
