97bbbec5b7aef4afd6a7706ec7516dcf05b1bed9
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-05-26 10:25:57 +0800

    Fetch display repos

--- 08_api_projects/01_github_finder/js/github.js
+++ 08_api_projects/01_github_finder/js/github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.client_id = "a4663834b17415635e72";
     this.client_secret = "8c49504f92183d2d60dfe5429bb3bd08d454e2ea";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -10,11 +12,18 @@ class Github {
         this.client_id
       }&client_secret=${this.client_secret}`
     );
-
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
     const profile = await profileResponse.json();
-
+    const repos = await repoResponse.json();
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
